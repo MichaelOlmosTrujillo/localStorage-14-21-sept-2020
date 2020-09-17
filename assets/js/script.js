fdd068e4ee7f4bcf58fc0a9cec615145e0994476
Michael Olmos Trujillo <molmost@unal.edu.co>
2020-09-17 00:16:31 -0500

    Local Storage del darkmode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-const dark = document.getElementById('dark')
+    let darkMode = localStorage.getItem('darkMode');
+    const dark = document.getElementById('dark')
+    const paginaDark = document.getElementById('container');
 
-dark.addEventListener('click', ()=>{
-    document.getElementById('container').classList.add('dark')
-})
+
+    const habilitarDarkMode = () => {
+
+        paginaDark.classList.add('dark');
+        localStorage.setItem('darkMode', 'habilitado');
+    }
+    const desabilitarDarkMode = () => {
+        paginaDark.classList.remove('dark');
+        localStorage.setItem('darkMode', null);
+    }
+    if (darkMode === 'habilitado') {
+        habilitarDarkMode();
+    }
+    dark.addEventListener('click', () => {
+        let darkMode = localStorage.getItem('darkMode');
+        if (darkMode !== 'habilitado') {
+            habilitarDarkMode();
+        } else {
+            desabilitarDarkMode();
+        }
+    })
